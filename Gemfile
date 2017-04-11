4f2c4372e97d3b53184d48a5cbb28313b904ce14
yarzin <zinyaroslav@gmail.com>
2017-04-11 17:41:25 +0300

    add api documentation

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+ruby ' 2.3.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
@@ -21,7 +21,7 @@ gem 'puma', '~> 3.0'
 #add gems
 gem 'active_model_serializers'
 gem 'rack-cors'
-
+gem 'apipie-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/api/v1/readmes_controller.rb
+++ app/controllers/api/v1/readmes_controller.rb
@@ -1,12 +1,38 @@
 class Api::V1::ReadmesController < ApplicationController
 
+
+  resource_description do
+    short "API for managing Readme"
+  end
+
+  api :GET, '/readmes', 'All readmes '
+  description <<-EOS
+    == Get Action Readmes
+    Is used for show readmes
+      Is used for get all readmes
+        curl localhost:3000/api/v1/readmes
+  EOS
   def index
     render json: Readme.all
   end
 
+  api :GET, '/readmes/:id', 'Show readme'
+  description <<-EOS
+    == Show single readme
+    Is used for show single readme
+      curl -v localhost:3000/api/v1/readmes/1
+  EOS
+
   def show
     render json: readme
   end
+
+  api :POST, '/readmes', 'Create readme'
+  description <<-EOS
+    == Create readme article
+    Is used for creating book
+      curl -v localhost:3000/api/v1/readmes -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"text": "some text test"}'
+  EOS
 
   def create
     render json: Readme.create(readme_params)
@@ -15,6 +41,13 @@ class Api::V1::ReadmesController < ApplicationController
   def update
     render json: readme.update(readme_params)
   end
+
+  api :DELETE, '/readmes/:id', 'Delete readme'
+  description <<-EOS
+	  == Delete single readme
+	  Is used for delete single readme
+	    curl -v localhost:3000/api/v1/readmes/1 -X DELETE -H "Accept: application/json" -H "Content-Type: application/json"
+  EOS
 
   def destroy
     render json: readme.destroy
